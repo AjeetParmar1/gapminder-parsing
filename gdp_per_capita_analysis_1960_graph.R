f7fc6be7dc904108f1d085fdd08ef2acf51d7b05
Ajeet Parmar <ajeetparmar45@gmail.com>
2020-07-13 23:37:37 -0400

    Updated GDP per capita analysis

--- gdp_per_capita_analysis_1960_graph.R
+++ gdp_per_capita_analysis_1960_graph.R
@@ -10,4 +10,5 @@ transitional <- c("Russia", "India", "Brazil", "China", "South Africa")
 gapminder %>% filter(year == 1960) %>%
   mutate(group = ifelse(region %in% west, "West", ifelse(country %in% transitional, "transitional", "Developing"))) %>%
   ggplot(aes(y = gdp/population, x = group, color = group)) +
-  geom_boxplot()  
+  geom_boxplot() + ggtitle("GDP per capita in 1960") + xlab("Group") +
+  ylab("GDP per capita (in USD)")
